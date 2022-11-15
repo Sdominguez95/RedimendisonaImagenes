9804fc9631e542bc31559a9699a865c6a5b5ad2e
Segundo Eduardo Dominguez Rios <sdominguez@andesindustrial.cl>
2022-11-15 17:19:48 -0300

    Incluir codigo For

--- RedimendisonaImagenes/Program.cs
+++ RedimendisonaImagenes/Program.cs
@@ -7,9 +7,8 @@ namespace RedimendisonaImagenes
     {
         static void Main(string[] args)
         {
-
-            string RutaS = @"D:\Proyectos\RedimendisonaImagenes\RedimendisonaImagenes\test\leon.jpg";
-            string RutaG = @"D:\Proyectos\RedimendisonaImagenes\RedimendisonaImagenes\test\leon_mini.jpg";
+            string RutaS = @"D:\Proyectos\RedimendisonaImagenes\RedimendisonaImagenes\test\lago.png";
+            string RutaG = @"D:\Proyectos\RedimendisonaImagenes\RedimendisonaImagenes\test\lago.jpg";
             using(MagickImage oMagickImage = new MagickImage(RutaS))
             {
                 oMagickImage.Resize(1600, 0);
@@ -20,4 +19,18 @@ namespace RedimendisonaImagenes
 }
 
 
-
+string basePATH = $"D:\\PublicacionesAWS\\Produccion\\Andes\\imgs\\imgsPreview\\{request.IDPublicacion}";
+string[] folders = Directory.GetDirectories(basePATH);
+string path = "";
+foreach (string folder in folders)
+{
+    path = folder;
+    string[] files = Directory.GetFiles(path); // Obtener archivos
+    foreach (string file in files)
+    {
+        string sourceFile = file;
+        string destinationFile = $"D:\\PublicacionesAWS\\Produccion\\Andes\\imgs\\{(folder).Replace(basePATH + '\\', "")}\\{file.Replace(folder + '\\', "")}";
+        if (!System.IO.File.Exists(destinationFile))
+        {
+            System.IO.File.Copy(sourceFile, destinationFile);
+        }
